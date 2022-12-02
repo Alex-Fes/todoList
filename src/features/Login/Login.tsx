58b3d2152deff07f7f9b11ab7a9ab0de75441d36
Alex-Fes <aleksei.fesenko86@gmail.com>
2022-12-02 22:14:11 +0100

    refactor little bit

--- src/features/Login/Login.tsx
+++ src/features/Login/Login.tsx
@@ -19,15 +19,14 @@ export const Login = () => {
     const isLoggedIn = useAppSelector(state => state.auth.isLoggedIn)
 
 
-
     const formik = useFormik({
         validate(values) {
-            if(!values.email) {
+            if (!values.email) {
                 return {
                     email: 'Email is required'
                 }
             }
-            if(!values.password) {
+            if (!values.password) {
                 return {
                     email: 'Password is required'
                 }
@@ -43,41 +42,41 @@ export const Login = () => {
 
         },
     });
-    if(isLoggedIn) {
-        return <Navigate to={'/'} />
+    if (isLoggedIn) {
+        return <Navigate to={'/'}/>
     }
     return <Grid container justifyContent={'center'}>
         <Grid item justifyContent={'center'}>
             <form onSubmit={formik.handleSubmit}>
-            <FormControl>
-                <FormLabel>
-                    <p>To log in get registered
-                        <a href={'https://social-network.samuraijs.com/'}
-                           target={'_blank'}> here
-                        </a>
-                    </p>
-                    <p>or use common test account credentials:</p>
-                    <p>Email: free@samuraijs.com</p>
-                    <p>Password: free</p>
-                </FormLabel>
-                <FormGroup>
-                    <TextField label="Email"
-                               margin="normal"
-                               {...formik.getFieldProps('email')}/>
-                    {formik.errors.email ? <div>{formik.errors.email}</div> : null}
-                    <TextField type="password" label="Password"
-                               margin="normal"
-                               {...formik.getFieldProps('password')}/>
-                    {formik.errors.password ? <div>{formik.errors.password}</div> : null}
-                    <FormControlLabel label={'Remember me'}
-                                      control={<Checkbox
-                                          {...formik.getFieldProps('rememberMe')}
-                                      checked={formik.values.rememberMe}/>}/>
-                    <Button type={'submit'} variant={'contained'} color={'primary'}>
-                        Login
-                    </Button>
-                </FormGroup>
-            </FormControl>
+                <FormControl>
+                    <FormLabel>
+                        <p>To log in get registered
+                            <a href={'https://social-network.samuraijs.com/'}
+                               target={'_blank'}> here
+                            </a>
+                        </p>
+                        <p>or use common test account credentials:</p>
+                        <p>Email: free@samuraijs.com</p>
+                        <p>Password: free</p>
+                    </FormLabel>
+                    <FormGroup>
+                        <TextField label="Email"
+                                   margin="normal"
+                                   {...formik.getFieldProps('email')}/>
+                        {formik.errors.email ? <div>{formik.errors.email}</div> : null}
+                        <TextField type="password" label="Password"
+                                   margin="normal"
+                                   {...formik.getFieldProps('password')}/>
+                        {formik.errors.password ? <div>{formik.errors.password}</div> : null}
+                        <FormControlLabel label={'Remember me'}
+                                          control={<Checkbox
+                                              {...formik.getFieldProps('rememberMe')}
+                                              checked={formik.values.rememberMe}/>}/>
+                        <Button type={'submit'} variant={'contained'} color={'primary'}>
+                            Login
+                        </Button>
+                    </FormGroup>
+                </FormControl>
             </form>
         </Grid>
     </Grid>
