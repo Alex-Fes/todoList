430bcd7e8827e95660def84fefd8a1c8f86b4ead
Alex-Fes <aleksei.fesenko86@gmail.com>
2023-01-12 21:57:13 +0100

    refactor: create store with configureStore

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,32 +1,44 @@
-import {applyMiddleware, combineReducers, legacy_createStore as createStore} from "redux";
-import {TodolistActionType, todolistsReducer} from "../features/TodolistList/todolist-reduser";
-import {taskReducer, TasksActionType} from "../features/TodolistList/tasks-reduser";
-import thunkMiddleware, {ThunkAction, ThunkDispatch} from "redux-thunk";
-import {appReducer, AppReducerActionsType} from "./app-reducer";
-import {LoginActionType, authReducer} from "../features/Login/auth-reduser";
+import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers } from 'redux'
+import thunkMiddleware, { ThunkAction, ThunkDispatch } from 'redux-thunk'
 
+import { authReducer, LoginActionType } from '../features/Login/auth-reduser'
+import { taskReducer, TasksActionType } from '../features/TodolistList/tasks-reduser'
+import { TodolistActionType, todolistsReducer } from '../features/TodolistList/todolist-reduser'
 
+import { appReducer, AppReducerActionsType } from './app-reducer'
 
 const RootReducer = combineReducers({
-    todolists: todolistsReducer,
-    tasks: taskReducer,
-    app: appReducer,
-    auth: authReducer
+  todolists: todolistsReducer,
+  tasks: taskReducer,
+  app: appReducer,
+  auth: authReducer,
 })
 
-export const store = createStore(RootReducer, applyMiddleware(thunkMiddleware))
+//export const store = createStore(RootReducer, applyMiddleware(thunkMiddleware))
+
+export const store = configureStore({
+  reducer: RootReducer,
+  middleware: getDefaultMiddleware => getDefaultMiddleware().prepend(thunkMiddleware),
+})
 
 //types
 export type AppRootStateType = ReturnType<typeof store.getState>
 
 //all types of Action for all App
-export type AppActionsType = TodolistActionType
-                            | TasksActionType
-                            | AppReducerActionsType
-                            | LoginActionType;
+export type AppActionsType =
+  | TodolistActionType
+  | TasksActionType
+  | AppReducerActionsType
+  | LoginActionType
 
 //type for thunk
-export type AppThunkType<ReturnType = void> = ThunkAction<ReturnType, AppRootStateType, unknown, AppActionsType>
+export type AppThunkType<ReturnType = void> = ThunkAction<
+  ReturnType,
+  AppRootStateType,
+  unknown,
+  AppActionsType
+>
 
 //type for dispatch
 //export type AppDispatchType = typeof store.dispatch
