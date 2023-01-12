937466006b83a3aee69dd570e0256e442baa7d59
Alex-Fes <aleksei.fesenko86@gmail.com>
2023-01-12 21:56:30 +0100

    style: ESLint, refactor setAppStatusAC({})

--- src/utils/error-utils.ts
+++ src/utils/error-utils.ts
@@ -1,26 +1,25 @@
-import {setAppErrorAC, setAppStatusAC} from "../app/app-reducer";
-import {ResponseType} from "../api/todolists-api";
-import {AppDispatchType} from "../app/store";
-import {useAppDispatch} from "../app/hooks/hooks";
-
-
+import { ResponseType } from '../api/todolists-api'
+import { setAppErrorAC, setAppStatusAC } from '../app/app-reducer'
+import { AppDispatchType } from '../app/store'
 
 export const handleServerAppError = <D>(data: ResponseType<D>, dispatch: AppDispatchType) => {
-    if (data.messages.length) {
-        dispatch(setAppErrorAC(data.messages[0]))
-    } else {
-        dispatch(setAppErrorAC('Some error occurred'))
-    }
-    dispatch(setAppStatusAC("failed"))
+  if (data.messages.length) {
+    dispatch(setAppErrorAC({ error: data.messages[0] }))
+  } else {
+    dispatch(setAppErrorAC({ error: 'Some error occurred' }))
+  }
+  dispatch(setAppStatusAC({ status: 'failed' }))
 }
 
-export const handleNetworkAppError = (error: {message: string}, dispatch: AppDispatchType) => {
-    dispatch(setAppErrorAC(error.message? error.message : 'Some error occurred'))
-    dispatch(setAppStatusAC("failed"))
+export const handleNetworkAppError = (error: { message: string }, dispatch: AppDispatchType) => {
+  dispatch(
+    setAppErrorAC(error.message ? { error: error.message } : { error: 'Some error occurred' })
+  )
+  dispatch(setAppStatusAC({ status: 'failed' }))
 }
 
 export const checkLengthTitle = (title: string, dispatch: AppDispatchType) => {
-    if (title.length >= 100) {
-       return dispatch(setAppErrorAC('Your title must be short then 100 symbols'))
-    }
+  if (title.length >= 100) {
+    return dispatch(setAppErrorAC({ error: 'Your title must be short then 100 symbols' }))
+  }
 }
