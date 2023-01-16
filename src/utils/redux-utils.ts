310c9a59748dea2d24392140646d69714cbdcd33
Alex-Fes <aleksei.fesenko86@gmail.com>
2023-01-16 22:41:49 +0100

    add: add useActions for TC, take TC - return actions

---
+++ src/utils/redux-utils.ts
@@ -0,0 +1,13 @@
+import { useMemo } from 'react'
+
+import { ActionCreatorsMapObject, bindActionCreators } from 'redux'
+
+import { useAppDispatch } from '../app/hooks/hooks'
+
+export function useActions<T extends ActionCreatorsMapObject<any>>(actions: T) {
+  const dispatch = useAppDispatch()
+
+  return useMemo(() => {
+    return bindActionCreators(actions, dispatch)
+  }, [])
+}
